88607e9d67f92645b1b7a00c2fcb2a1322c64b63
ANDRES LOBO ROCA <s184590@student.hb.se>
2021-04-30 22:10:28 +0200

    first commit

---
+++ App1/App1/Views/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
